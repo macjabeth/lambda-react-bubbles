86e4f23f57bb685c04fc54363f4100547ab14260
macjabeth <macbethjonathan@gmail.com>
2019-10-11 23:00:20 -0400

    complete mvp

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,16 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
-import Login from "./components/Login";
-import "./styles.scss";
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { PrivateRoute } from './auth';
+import Login from './components/Login';
+import BubblePage from './components/BubblePage';
+import './styles.scss';
 
 function App() {
-  const [colorList, setColorList] = useState([]);
   return (
     <Router>
-      <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+      <div className='App'>
+        <Route exact path='/' component={Login} />
+        <PrivateRoute path='/bubbles' component={BubblePage} />
       </div>
     </Router>
   );
--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React, { useState } from 'react';
+import { serverHandshake } from '../auth';
 
 const initialColor = {
-  color: "",
-  code: { hex: "" }
+  color: '',
+  code: { hex: '' }
 };
 
 const ColorList = ({ colors, updateColors }) => {
@@ -16,31 +16,38 @@ const ColorList = ({ colors, updateColors }) => {
     setColorToEdit(color);
   };
 
-  const saveEdit = e => {
+  const saveEdit = async e => {
     e.preventDefault();
-    // Make a put request to save your updated color
-    // think about where will you get the id from...
-    // where is is saved right now?
+    await serverHandshake(true).put(`/colors/${colorToEdit.id}`, colorToEdit);
+    updateColors(colors.map(c => (c.id === colorToEdit.id ? colorToEdit : c)));
+    setEditing(false);
   };
 
-  const deleteColor = color => {
-    // make a delete request to delete this color
+  const deleteColor = async color => {
+    await serverHandshake(true).delete(`/colors/${color.id}`);
+    updateColors(colors.filter(c => c.id !== color.id));
   };
 
   return (
-    <div className="colors-wrap">
+    <div className='colors-wrap'>
       <p>colors</p>
       <ul>
         {colors.map(color => (
           <li key={color.color} onClick={() => editColor(color)}>
             <span>
-              <span className="delete" onClick={() => deleteColor(color)}>
+              <span
+                className='delete'
+                onClick={e => {
+                  e.stopPropagation();
+                  deleteColor(color);
+                }}
+              >
                 x
-              </span>{" "}
+              </span>{' '}
               {color.color}
             </span>
             <div
-              className="color-box"
+              className='color-box'
               style={{ backgroundColor: color.code.hex }}
             />
           </li>
@@ -70,13 +77,13 @@ const ColorList = ({ colors, updateColors }) => {
               value={colorToEdit.code.hex}
             />
           </label>
-          <div className="button-row">
-            <button type="submit">save</button>
+          <div className='button-row'>
+            <button type='submit'>save</button>
             <button onClick={() => setEditing(false)}>cancel</button>
           </div>
         </form>
       )}
-      <div className="spacer" />
+      <div className='spacer' />
       {/* stretch - build another form here to add a color */}
     </div>
   );
